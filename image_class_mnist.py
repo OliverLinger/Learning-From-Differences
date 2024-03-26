7f0f7e99bc5c07335daa982973064b6a80f0f09b
OliverLinger <olienew32@gmail.com>
2024-03-26 11:08:50 +0000

    Adding changes

--- image_class_mnist.py
+++ image_class_mnist.py
@@ -26,7 +26,7 @@ model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
 model.add(MaxPooling2D((2, 2)))
 model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D((2, 2)))
-model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'   ))
 model.add(Flatten())
 model.add(Dense(64, activation='relu'))
 model.add(Dense(10, activation='softmax'))  # Number of classes in MNIST is 10
